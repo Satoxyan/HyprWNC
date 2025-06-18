ea20f5dfa6d5590a4428a9b973bea1a99eb1145c
Satoxyan <ayangsatya321@gmail.com>
2025-06-18 23:49:02 +0700

    revert changes: wallselect layer

--- .config/ags/modules/bar/main.js
+++ .config/ags/modules/bar/main.js
@@ -93,7 +93,6 @@ export const Bar = async (monitor = 0) => {
         monitor,
         name: `bar${monitor}`,
         anchor: ['top', 'left', 'right'],
-        layer: 'overlay',
         exclusivity: 'exclusive',
         visible: true,
         child: Widget.Stack({
