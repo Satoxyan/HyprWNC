c4a7a604d459e0d6d22e32a345e284ebac92ba95
Satoxyan <168896653+Satoxyan@users.noreply.github.com>
2025-06-21 14:04:05 +0700

    lockscreen: add charging indicator

--- .config/hypr/hyprlock/battery.sh
+++ .config/hypr/hyprlock/battery.sh
@@ -5,13 +5,16 @@ enable_battery=false
 battery_charging=false
 battery_level=0
 battery_icon=""
+charging_icon=""
 
 ####### Check availability ########
 for battery in /sys/class/power_supply/*BAT*; do
   if [[ -f "$battery/uevent" ]]; then
     enable_battery=true
     battery_status=$(cat "$battery/status")
-    battery_charging=[[ "$battery_status" == "Charging" ]]
+    if [[ "$battery_status" == "Charging" ]]; then
+      battery_charging=true
+    fi
     battery_level=$(cat "$battery/capacity")
     break
   fi
@@ -20,32 +23,33 @@ done
 ########## Choose icon ###########
 if [[ $enable_battery == true ]]; then
   if (( battery_level >= 100 )); then
-    battery_icon="󰁹" # Full
+    battery_icon="󰁹"
   elif (( battery_level >= 90 )); then
-    battery_icon="󰂂"
+    battery_icon="󰂂"
   elif (( battery_level >= 80 )); then
-    battery_icon="󰂁"
+    battery_icon="󰂁"
   elif (( battery_level >= 70 )); then
-    battery_icon="󰂀"
+    battery_icon="󰂀"
   elif (( battery_level >= 60 )); then
-    battery_icon="󰁿"
+    battery_icon="󰁿"
   elif (( battery_level >= 50 )); then
-    battery_icon="󰁾"
+    battery_icon="󰁾"
   elif (( battery_level >= 40 )); then
-    battery_icon="󰁽"
+    battery_icon="󰁽"
   elif (( battery_level >= 30 )); then
-    battery_icon="󰁼"
+    battery_icon="󰁼"
   elif (( battery_level >= 20 )); then
-    battery_icon="󰁻"
+    battery_icon="󰁻"
   else
-    battery_icon="󰁺" # Low
+    battery_icon="󰁺"
+  fi
+
+  if [[ "$battery_charging" == true ]]; then
+    charging_icon="󱐋"
   fi
 fi
 
 ############# Output #############
 if [[ $enable_battery == true ]]; then
-  if [[ "$battery_charging" == true ]]; then
-    echo -n "(+) "
-  fi
-  echo "${battery_level}% ${battery_icon}"
+  echo "${battery_level}% ${battery_icon}${charging_icon}"
 fi
